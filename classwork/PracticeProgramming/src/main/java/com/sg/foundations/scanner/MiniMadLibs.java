fcb4bdf0c5e64647f42e410aa959075edb43a341
Jorge Villa <villajorge41@gmail.com>
2022-08-02 15:20:19 -0500

    Programming Prac 3

--- classwork/PracticeProgramming/src/main/java/com/sg/foundations/scanner/MiniMadLibs.java
+++ classwork/PracticeProgramming/src/main/java/com/sg/foundations/scanner/MiniMadLibs.java
@@ -4,7 +4,7 @@
  */
 
 package com.sg.foundations.scanner;
-
+import java.util.Scanner;
 /**
  * @author: Jorge Villa 
  * email: villajorge41@gmail.com
@@ -13,5 +13,37 @@ package com.sg.foundations.scanner;
  * 
  */
 public class MiniMadLibs {
-
+    public static void main(String[] args){
+        String noun1, adj1, noun2, adj2, pluralNoun1, pluralNoun2, pluralNoun3, verbPresent, verbPast;  
+        int num;
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Lets play MAD LIBS!");
+        System.out.println("I need a noun: ");
+        noun1 = scan.nextLine();
+        System.out.println("Now an adjective: ");
+        adj1 = scan.nextLine();
+        System.out.println("Another noun: ");
+        noun2 = scan.nextLine();
+        System.out.println("And a number: ");
+        num = Integer.parseInt(scan.nextLine());
+        System.out.println("Another adjective: ");
+        adj2 = scan.nextLine();
+        System.out.println("A plural noun: ");
+        pluralNoun1 = scan.nextLine();
+        System.out.println("Another one: ");
+        pluralNoun2 = scan.nextLine();
+        System.out.println("One more: ");
+        pluralNoun3 = scan.nextLine();
+        System.out.println("A verb (present tense): ");
+        verbPresent = scan.nextLine();
+        System.out.println("Same verb (past tense): ");
+        verbPast = scan.nextLine();
+        
+        System.out.println("*****MAD LIB*****");
+        System.out.println(noun1+": the "+adj1+" fronteir. These are the voyages of the starship "+noun2+".");
+        System.out.println("Its "+num+"-year mission: to explore strange "+adj2+" "+pluralNoun1+",");
+        System.out.println("to seek out "+adj2+" "+pluralNoun2+" and "+adj2+" "+pluralNoun3+",");
+        System.out.println("to boldly "+verbPresent+" where no one has "+verbPast+" before.");
+    }
 }
